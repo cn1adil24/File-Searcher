cf969812424344112fd25ab5dcc8221991dbc3f5
cn1adil24 <shehmeeradil123@gmail.com>
2020-01-19 21:04:59 +0500

    Added Project

---
+++ FileSearcher/FileSearcher/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace FileSearcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.search = new System.Windows.Forms.Button();
+            this.search_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_check = new System.Windows.Forms.CheckBox();
+            this.doc_check = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.csv_check = new System.Windows.Forms.CheckBox();
+            this.xml_check = new System.Windows.Forms.CheckBox();
+            this.json_check = new System.Windows.Forms.CheckBox();
+            this.advanced_check = new System.Windows.Forms.CheckBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 627);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(534, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 598);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "..";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = ".";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(9, 113);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(102, 20);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Select Folder";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(8, 136);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(546, 28);
+            this.panel1.TabIndex = 9;
+            // 
+            // search
+            // 
+            this.search.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search.Location = new System.Drawing.Point(472, 220);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(89, 38);
+            this.search.TabIndex = 5;
+            this.search.Text = "Search";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // search_box
+            // 
+            this.search_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search_box.Location = new System.Drawing.Point(12, 72);
+            this.search_box.Name = "search_box";
+            this.search_box.Size = new System.Drawing.Size(549, 27);
+            this.search_box.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Enter Keyword:";
+            // 
+            // txt_check
+            // 
+            this.txt_check.AutoSize = true;
+            this.txt_check.Location = new System.Drawing.Point(11, 237);
+            this.txt_check.Name = "txt_check";
+            this.txt_check.Size = new System.Drawing.Size(49, 21);
+            this.txt_check.TabIndex = 12;
+            this.txt_check.Text = "Txt";
+            this.txt_check.UseVisualStyleBackColor = true;
+            // 
+            // doc_check
+            // 
+            this.doc_check.AutoSize = true;
+            this.doc_check.Location = new System.Drawing.Point(120, 237);
+            this.doc_check.Name = "doc_check";
+            this.doc_check.Size = new System.Drawing.Size(55, 21);
+            this.doc_check.TabIndex = 13;
+            this.doc_check.Text = "Doc";
+            this.doc_check.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Select type(s):";
+            // 
+            // csv_check
+            // 
+            this.csv_check.AutoSize = true;
+            this.csv_check.Location = new System.Drawing.Point(239, 237);
+            this.csv_check.Name = "csv_check";
+            this.csv_check.Size = new System.Drawing.Size(53, 21);
+            this.csv_check.TabIndex = 15;
+            this.csv_check.Text = "Csv";
+            this.csv_check.UseVisualStyleBackColor = true;
+            // 
+            // xml_check
+            // 
+            this.xml_check.AutoSize = true;
+            this.xml_check.Location = new System.Drawing.Point(11, 274);
+            this.xml_check.Name = "xml_check";
+            this.xml_check.Size = new System.Drawing.Size(58, 21);
+            this.xml_check.TabIndex = 16;
+            this.xml_check.Text = "XML";
+            this.xml_check.UseVisualStyleBackColor = true;
+            // 
+            // json_check
+            // 
+            this.json_check.AutoSize = true;
+            this.json_check.Location = new System.Drawing.Point(120, 274);
+            this.json_check.Name = "json_check";
+            this.json_check.Size = new System.Drawing.Size(67, 21);
+            this.json_check.TabIndex = 17;
+            this.json_check.Text = "JSON";
+            this.json_check.UseVisualStyleBackColor = true;
+            // 
+            // advanced_check
+            // 
+            this.advanced_check.AutoSize = true;
+            this.advanced_check.Enabled = false;
+            this.advanced_check.Location = new System.Drawing.Point(12, 170);
+            this.advanced_check.Name = "advanced_check";
+            this.advanced_check.Size = new System.Drawing.Size(142, 21);
+            this.advanced_check.TabIndex = 18;
+            this.advanced_check.Text = "Advanced Search";
+            this.advanced_check.UseVisualStyleBackColor = true;
+            this.advanced_check.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Cambria", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(472, 170);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(89, 38);
+            this.clear.TabIndex = 19;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(577, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(976, 653);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1560, 672);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.advanced_check);
+            this.Controls.Add(this.json_check);
+            this.Controls.Add(this.xml_check);
+            this.Controls.Add(this.csv_check);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.doc_check);
+            this.Controls.Add(this.txt_check);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.search_box);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 5, 0);
+            this.Text = "Search";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.TextBox search_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox txt_check;
+        private System.Windows.Forms.CheckBox doc_check;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox csv_check;
+        private System.Windows.Forms.CheckBox xml_check;
+        private System.Windows.Forms.CheckBox json_check;
+        private System.Windows.Forms.CheckBox advanced_check;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ FileSearcher/FileSearcher/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ookii.Dialogs.WinForms;
+using System.Text.RegularExpressions;
+
+namespace FileSearcher
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            txt_check.Hide();
+            doc_check.Hide();
+            csv_check.Hide();
+            xml_check.Hide();
+            json_check.Hide();
+            //progress_label.Hide();
+            progressBar1.Hide();
+        }        
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(path))
+            {
+                MessageBox.Show("Please select a folder!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (String.IsNullOrEmpty(search_box.Text) || String.IsNullOrWhiteSpace(search_box.Text))
+            {
+                MessageBox.Show("Please enter a keyword!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                List<Result> list = new List<Result>();
+                try
+                {
+                    // Get folders of root directory, e.g folders in C:/Users
+                    var directories = Directory.GetDirectories(path);                    
+                    List<string> files = new List<string>();                    
+                    progressBar1.Show();
+                    label2.Show();
+                    if (directories.Length != 0)
+                    {
+                        List<string> tree = new List<string>();
+                        tree.Add(path);
+                        //progressBar1.Show();
+                        //label2.Show();
+
+                        progressBar1.Maximum = Math.Max(directories.Length, 10000);
+                        progressBar1.Step = 1;
+                        label2.Text = "Extracting folders..";
+                        label2.Refresh();
+                        // Extract folders and its nested sub-directories
+                        foreach (var folder in ExtractFolders(path))
+                        {
+                            tree.Add(folder);
+                            if (progressBar1.Value + 1 == progressBar1.Maximum)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                progressBar1.PerformStep();
+                                progressBar1.Refresh();
+                            }
+                        }
+                        progressBar1.PerformStep();
+
+                        // Extract all files from nested folders
+                        //List<string> files = new List<string>();
+                        progressBar1.Value = 0;
+                        progressBar1.Maximum = tree.Count;
+                        progressBar1.Step = 1;
+                        label2.Text = "Extracting files..";
+                        label2.Refresh();
+                        foreach (var folder in tree)
+                        {
+                            foreach(var file in ExtractFiles(folder))
+                            {
+                                files.Add(file);
+                            }
+                            progressBar1.PerformStep();
+                            progressBar1.Refresh();
+                        }                        
+                    }
+                    else
+                    {
+                        progressBar1.Maximum = Directory.GetFiles(path).Length;
+                        progressBar1.Step = 1;
+                        label2.Text = "Extracting files..";
+                        label2.Refresh();
+                        foreach (var file in ExtractFiles(path))
+                        {
+                            files.Add(file);
+                            progressBar1.PerformStep();
+                            progressBar1.Refresh();
+                        }
+                    }
+                    List<Result> search_results = new List<Result>();
+                    progressBar1.Value = 0;
+                    progressBar1.Maximum = files.Count;
+                    progressBar1.Step = 1;
+                    label2.Text = "Searching..";
+                    label2.Refresh();
+
+                    // Search file names
+                    foreach (var file in files)
+                    {
+                        var file_name = Path.GetFileName(file);
+                        Match m = Regex.Match(file_name.ToUpper(), search_box.Text.ToUpper());
+                        if (m.Success)
+                        {
+                            search_results.Add(new Result(
+                                                          Path.GetFileNameWithoutExtension(file),
+                                                          file,
+                                                          Path.GetExtension(file))
+                                              );
+                            count++;
+                        }
+                        progressBar1.PerformStep();
+                        progressBar1.Refresh();
+                    }
+                    if (count != 0)
+                    {
+                        dataGridView1.DataSource = search_results;
+                        MessageBox.Show("Found " + count.ToString() + " file(s)", "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = null;
+                        MessageBox.Show("No such file exists!", "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);                        
+                    }
+                    label2.Hide();
+                    progressBar1.Value = 0;
+                    progressBar1.Hide();
+                    count = 0;
+
+                    if (advanced_check.Checked)
+                    {
+                        // Advanced Search: through all files
+                        foreach (var file in files)
+                        {
+                            if (Path.GetExtension(file).Equals(".txt") || Path.GetExtension(file).Equals(".doc"))
+                            {
+                                // simple read
+                            }
+                            else if (Path.GetExtension(file).Equals(".csv"))
+                            {
+                                // CSVParser
+                            }
+                            else if (Path.GetExtension(file).Equals(".xml"))
+                            {
+                                // XMLParser
+                            }
+                            else if (Path.GetExtension(file).Equals(".json"))
+                            {
+                                // JSONParser
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var dialog = new VistaFolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                path = dialog.SelectedPath;
+                label3.Text = "Folder selected: " + path;
+                label3.Show();
+            }
+        }
+        private List<string> ExtractFolders(string folder)
+        {
+            List<string> tree = new List<string>();
+            GetFolders(folder, tree);
+            return tree;
+        }
+        private void GetFolders(string folder, List<string> tree)
+        {
+            var hierarchy = Directory.GetDirectories(folder);
+            if (hierarchy != null)
+            {
+                /* Select each folder from the root path
+                   For example C:/Users/Abc -> Target: 'Abc'
+                               C:/Users/Xyz -> Target: 'Xyz' */
+                foreach (var f in hierarchy)
+                {
+                    tree.Add(f);
+                    /* Extract sub-folders of each folder
+                       For example C:/Users/Abc/A -> Target 'A' and its sub-directories recursively
+                                   C:/Users/Abc/B -> Target 'B' and its sub-directories recursively */
+                    GetFolders(f, tree);
+                }
+            }
+            return;
+        }
+        private List<string> ExtractFiles(string folder)
+        {
+            List<string> files = new List<string>();
+            var all_files = Directory.GetFiles(folder);
+            foreach (var file in all_files)
+            {
+                files.Add(file);
+            }
+            return files;
+        }
+        private List<string> ExtractAdvancedFiles(string folder)
+        {
+            List<string> files = new List<string>();
+            if (txt_check.Checked)
+            {
+                var txt_files = Directory.GetFiles(folder, "*.txt");
+                // Extract all txt files
+                foreach (var txt in txt_files)
+                {
+                    files.Add(txt);
+                }
+            }
+            if (doc_check.Checked)
+            {
+                var doc_files = Directory.GetFiles(folder, "*.doc");
+                // Extract all doc files
+                foreach (var doc in doc_files)
+                {
+                    files.Add(doc);
+                }
+            }
+            if (csv_check.Checked)
+            {
+                var csv_files = Directory.GetFiles(folder, "*.csv");
+                // Extract all csv files
+                foreach (var csv in csv_files)
+                {
+                    files.Add(csv);
+                }
+            }
+            if (xml_check.Checked)
+            {
+                var xml_files = Directory.GetFiles(folder, "*.xml");
+                // Extract all xml files
+                foreach (var xml in xml_files)
+                {
+                    files.Add(xml);
+                }
+            }
+            if (json_check.Checked)
+            {
+                var json_files = Directory.GetFiles(folder, "*.json");
+                // Extract all json files
+                foreach (var json in json_files)
+                {
+                    files.Add(json);
+                }
+            }
+            return files;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (advanced_check.Checked)
+            {
+                label4.Show();
+                txt_check.Show();
+                doc_check.Show();
+                csv_check.Show();
+                xml_check.Show();
+                json_check.Show();
+            }
+            else
+            {
+                label4.Hide();
+                txt_check.Hide();
+                doc_check.Hide();
+                csv_check.Hide();
+                xml_check.Hide();
+                json_check.Hide();
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            search_box.Text = "";
+            label3.Text = "";
+            label3.Hide();
+            path = "";
+            label2.Hide();            
+            progressBar1.Hide();
+            progressBar1.Value = 0;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                string path = dataGridView1[1, e.RowIndex].Value.ToString();
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+                {
+                    FileName = path,
+                    UseShellExecute = true,
+                    Verb = "open"
+                });
+            }            
+        }
+    }
+    public class Result
+    {
+        public string name { get; set; }
+        public string path { get; set; }
+        public string type { get; set; }
+        public Result()
+        {
+        }
+        public Result(string name, string path, string type)
+        {
+            this.name = name;
+            this.path = path;
+            this.type = type;
+        }
+    }
+}
